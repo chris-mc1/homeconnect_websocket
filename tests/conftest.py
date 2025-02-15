317110e0a0824364fb3825afe1fc146339f4333b
chris-mc1 <29151206+chris-mc1@users.noreply.github.com>
2025-02-15 17:37:46 +0000

    add tests

---
+++ tests/conftest.py
@@ -0,0 +1,101 @@
+from __future__ import annotations
+
+import ssl
+from base64 import urlsafe_b64decode
+from typing import TYPE_CHECKING
+
+import pytest_asyncio
+from aiohttp import web
+from aiohttp.test_utils import TestServer
+from homeconnect_websocket.testutils import TEST_IV64, TEST_PSK64
+
+from utils import ApplianceServer, ApplianceServerAes
+
+if TYPE_CHECKING:
+    from collections.abc import AsyncGenerator, Awaitable, Callable
+
+pytest_plugins = ["aiohttp.pytest_plugin", "homeconnect_websocket.testutils"]
+
+
+@pytest_asyncio.fixture
+async def appliance_server() -> AsyncGenerator[
+    Callable[..., Awaitable[ApplianceServer]]
+]:
+    """Appliance Server with TLS."""
+    servers: list[TestServer] = []
+
+    async def go(port: int = 80) -> ApplianceServer:
+        appliance = ApplianceServer(None)
+
+        app = web.Application()
+        app.add_routes([web.get("/homeconnect", appliance.websocket_handler)])
+        test_server = TestServer(app, port=port)
+        await test_server.start_server()
+        appliance.host = test_server.host
+
+        servers.append(test_server)
+        return appliance
+
+    yield go
+
+    while servers:
+        await servers.pop().close()
+
+
+@pytest_asyncio.fixture
+async def appliance_server_tls() -> AsyncGenerator[
+    Callable[..., Awaitable[ApplianceServer]]
+]:
+    """Appliance Server with TLS."""
+    servers: list[TestServer] = []
+
+    async def go(psk64: str = TEST_PSK64, port: int = 443) -> ApplianceServer:
+        appliance = ApplianceServer(psk64)
+
+        psk = urlsafe_b64decode(psk64 + "===")
+        ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+        ssl_context.maximum_version = ssl.TLSVersion.TLSv1_2
+        ssl_context.set_ciphers("ALL")
+        ssl_context.check_hostname = False
+        ssl_context.set_psk_server_callback(lambda _: psk)
+
+        app = web.Application()
+        app.add_routes([web.get("/homeconnect", appliance.websocket_handler)])
+        test_server = TestServer(app, port=port)
+        await test_server.start_server(ssl=ssl_context)
+        appliance.host = test_server.host
+
+        servers.append(test_server)
+        return appliance
+
+    yield go
+
+    while servers:
+        await servers.pop().close()
+
+
+@pytest_asyncio.fixture
+async def appliance_server_aes() -> AsyncGenerator[
+    Callable[..., Awaitable[ApplianceServerAes]]
+]:
+    """Appliance Server with AES."""
+    servers: list[TestServer] = []
+
+    async def go(
+        psk64: str = TEST_PSK64, iv64: str = TEST_IV64, port: int = 80
+    ) -> ApplianceServer:
+        appliance = ApplianceServerAes(psk64, iv64)
+
+        app = web.Application()
+        app.add_routes([web.get("/homeconnect", appliance.websocket_handler)])
+        test_server = TestServer(app, port=port)
+        await test_server.start_server()
+        appliance.host = test_server.host
+
+        servers.append(test_server)
+        return appliance
+
+    yield go
+
+    while servers:
+        await servers.pop().close()
---
+++ tests/const.py
@@ -0,0 +1,43 @@
+SESSION_ID = 10
+SERVER_MESSAGE_ID = 20
+CLIENT_MESSAGE_ID = 30
+
+MOCK_APPLIANCE_INFO = {
+    "brand": "Fake_Brand",
+    "type": "HomeAppliance",
+    "deviceID": "Fake_deviceID",
+    "vib": "Fake_vib",
+    "haVersion": "1.1",
+    "hwVersion": "2.2",
+    "swVersion": "3.3",
+    "mac": "C8-D7-78-43-F2-23",
+    "serialNumber": "Fake_serialNumber",
+}
+
+SERVICE_VERSIONS = [
+    {"service": "ci", "version": 3},
+    {"service": "ei", "version": 2},
+    {"service": "iz", "version": 1},
+    {"service": "ni", "version": 1},
+    {"service": "ro", "version": 1},
+]
+
+NZ_INFO = {
+    "deviceID": "240210038618500561",
+    "eNumber": "FFAA1234/11",
+    "brand": "HomeConnectWS",
+    "vib": "FFAA1234",
+    "mac": "23-43-F2-78-D7-23",
+    "haVersion": "1.1",
+    "swVersion": "1.2.12.20211004153146",
+    "hwVersion": "1.1.0.5",
+    "deviceType": "Dishwasher",
+    "deviceInfo": "",
+    "customerIndex": "11",
+    "serialNumber": "240210038618500561",
+    "fdString": "0686",
+    "shipSki": "FBD7EDDF6A5BB504533D5397532FF21C1AFEF706",
+}
+
+DESCRIPTION_CHANGES = {}
+MANDATORY_VALUES = {}
