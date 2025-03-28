0061975195fb621e58d4a5379688db882e1510ca
chris-mc1 <29151206+chris-mc1@users.noreply.github.com>
2025-03-28 17:13:24 +0000

    lower timeout increase factor

--- src/homeconnect_websocket/const.py
+++ src/homeconnect_websocket/const.py
@@ -5,7 +5,7 @@ from typing import Final
 DEFAULT_HANDSHAKE_TIMEOUT: Final[int] = 60
 DEFAULT_SEND_TIMEOUT: Final[int] = 20
 MAX_CONNECT_TIMEOUT: Final[int] = 60
-TIMEOUT_INCREASE_FACTOR: Final[float] = 2.0
+TIMEOUT_INCREASE_FACTOR: Final[float] = 1.2
 
 # CID to types mapping for Description Parser
 DESCRIPTION_TYPES: Final = {
