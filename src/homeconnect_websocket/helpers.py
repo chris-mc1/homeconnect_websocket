6c41d30f79d73a5a87a495cff19bb76e5e2569a3
chris-mc1 <29151206+chris-mc1@users.noreply.github.com>
2025-03-31 15:25:16 +0000

    expand convert_bool to cover int and int as str

--- src/homeconnect_websocket/helpers.py
+++ src/homeconnect_websocket/helpers.py
@@ -2,17 +2,21 @@
 
 from __future__ import annotations
 
+import contextlib
 
-def convert_bool(obj: str | bool) -> bool:
+
+def convert_bool(obj: str | bool | float) -> bool:
     """Convert a string to as bool."""
     if isinstance(obj, str):
         if obj.lower() == "true":
             return True
         if obj.lower() == "false":
             return False
-        msg = "Can't convert %s to bool"
-        raise TypeError(msg, obj)
+        with contextlib.suppress(ValueError):
+            obj = float(obj)
     if isinstance(obj, bool):
         return obj
+    if isinstance(obj, float | int):
+        return bool(obj)
     msg = "Can't convert %s to bool"
     raise TypeError(msg, obj)
